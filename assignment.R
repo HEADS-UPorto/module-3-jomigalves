c6981f79c9a75fa0014fae3ba497937751932418
jomigalves <jma.alves@zoho.com>
2020-10-28 07:17:21 +0000

    Assignment 3

--- assignment.R
+++ assignment.R
@@ -5,8 +5,8 @@ data_list <- list(
   name = c("Buffalo Bill", "Calamity Jane", "Jesse James", "Annie Oakley", "Wyatt Earp"),
   sex = factor(c("male", "female", "male", "female", "male")),
   obs = list(
-    moustache = c(TRUE, FALSE, FALSE, FALSE),
-    criminal = c(FALSE, FALSE, TRUE, FALSE)
+    moustache = c(TRUE, FALSE, FALSE, FALSE, FALSE),
+    criminal = c(FALSE, FALSE, TRUE, FALSE, FALSE)
   )
 )
 
@@ -14,6 +14,7 @@ data_list <- list(
 data_frame <- as.data.frame(data_list)
 
 # now remove the `obs` from `data_list` before running the next command
+data_list$obs = NULL
 new_data <- as.data.frame(data_list)
 
 save(data_frame, new_data, file = "output.rda")
